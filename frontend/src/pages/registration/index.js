d55e3ab56e68a2e94cdd60d8f787e8cbe826fff2
arup-kumar <oritrokumar1234@gmail.com>
2023-02-06 16:06:06 +0600

    registration page responsive has been completed

--- frontend/src/pages/registration/index.js
+++ frontend/src/pages/registration/index.js
@@ -96,23 +96,23 @@ function Registration() {
   };
 
   return (
-    <div className="flex">
+    <div className="md:flex">
       {/* Left-side */}
       <ToastContainer />
-      <div className="w-2/4 flex flex-col items-end">
-        <div className="mr-[69px] mt-[150px]">
-          <h1 className="font-nunito font-bold text-[34px] text-[#11175D] mb-[13px]">
+      <div className="w-full px-5 md:px-0 md:w-2/4 md:flex md:flex-col md:items-end">
+        <div className="  md:mr-[50px] laptop:mr-[69px] mt-[100px] md:mt-[50px] laptop:mt-[100px] desktop:mt-[150px]">
+          <h1 className="font-nunito font-bold text-[20px] laptop:text-[25px] desktop:text-[34px] text-[#11175D]  desktop:mb-[13px]">
             Get started with easily register
           </h1>
-          <p className="font-nunito font-regular text-[20.34px] text-[#000000] opacity-[0.5] mb-[61px]">
+          <p className="font-nunito font-regular text-[15px] laptop:text-[20.34px] text-[#000000] opacity-[0.5] mb-[30px] md:mb-[40px]">
             Free register and you can enjoy it
           </p>
 
           {/* Email input part here */}
-          <div className="relative">
+          <div className="relative mt-[80px] md:mt-0">
             {emailError ? (
               <input
-                className=" border-[1.72px] border-solid rounded-lg border-red-600 w-[368px] h-20 pl-[52px] font-nunito font-semibold text-red-600 text-xl outline-none  mb-14  placeholder:text-red-600"
+                className=" border-[1.72px] border-solid rounded-lg border-red-600 w-full laptop:w-[280px] desktop:w-[368px]  py-[10px] md:py-[15px] px-[30px] desktop:h-20 desktop:pl-[52px] font-nunito font-semibold text-red-600 text-[13px] laptop:text-[15px] desktop:text-xl outline-none mb-8 placeholder:text-red-600"
                 type="email"
                 value={email}
                 placeholder={emailError}
@@ -120,7 +120,7 @@ function Registration() {
               />
             ) : (
               <input
-                className=" border-[1.72px] border-solid rounded-lg border-[#11175D] w-[368px] border-opacity-[0.3] h-20 pl-[52px] font-nunito font-semibold text-[#11175D] text-xl outline-1 outline-primary mb-14"
+                className=" border-[1.72px] border-solid rounded-lg border-[#11175D] w-full laptop:w-[280px] desktop:w-[368px] border-opacity-[0.3] py-[10px] md:py-[15px] px-[30px] desktop:h-20 desktop:pl-[52px] font-nunito font-semibold text-[#11175D] text-[13px] laptop:text-[15px] desktop:text-xl outline-1 outline-primary mb-8 "
                 type="email"
                 value={email}
                 onChange={handleEmail}
@@ -128,11 +128,11 @@ function Registration() {
             )}
 
             {emailError ? (
-              <p className="absolute top-[-12px] left-[34.4px] px-[18.2px] bg-white font-nunito font-regular text-red-600  ">
+              <p className="absolute top-[-8px] laptop:top-[-8px] desktop:top-[-12px] left-[20px] px-[10px] desktop:left-[34.4px] desktop:px-[18.2px] text-[10px] laptop:text-[11px] desktop:text-[13.76px]  bg-white font-nunito font-regular  text-red-600  ">
                 Email Address
               </p>
             ) : (
-              <p className="absolute top-[-12px] left-[34.4px] px-[18.2px] bg-white font-nunito font-regular text-[13.76px] text-[#11175D] text-opacity-[70%]">
+              <p className="absolute top-[-8px] laptop:top-[-8px] desktop:top-[-12px] left-[20px] px-[10px] laptop:left-[25px] desktop:left-[34.4px] desktop:px-[18.2px] text-[10px] laptop:text-[11px] desktop:text-[13.76px] bg-white font-nunito font-regular  text-[#11175D] text-opacity-[70%]">
                 Email Address
               </p>
             )}
@@ -142,7 +142,7 @@ function Registration() {
           <div className="relative">
             {nameError ? (
               <input
-                className=" border-[1.72px] border-solid rounded-lg border-red-600 w-[368px]  h-20 pl-[52px] font-nunito font-semibold text-red-600 text-xl  mb-14  outline-none placeholder:text-red-600 "
+                className=" border-[1.72px] border-solid rounded-lg border-red-600 w-full laptop:w-[280px] desktop:w-[368px]  py-[10px] md:py-[15px] px-[30px] desktop:h-20 desktop:pl-[52px] font-nunito font-semibold text-red-600 text-[13px] laptop:text-[15px] desktop:text-xl outline-none mb-8 placeholder:text-red-600 "
                 placeholder={nameError}
                 type="text"
                 value={name}
@@ -150,28 +150,28 @@ function Registration() {
               />
             ) : (
               <input
-                className=" border-[1.72px] border-solid rounded-lg border-[#11175D] w-[368px] border-opacity-[0.3] h-20 pl-[52px] font-nunito font-semibold text-[#11175D] text-xl  mb-14 outline-primary outline-1 "
+                className=" border-[1.72px] border-solid rounded-lg border-[#11175D] w-full laptop:w-[280px] desktop:w-[368px] border-opacity-[0.3] py-[10px] md:py-[15px] px-[30px] desktop:h-20 desktop:pl-[52px] font-nunito font-semibold text-[#11175D] text-[13px] laptop:text-[15px] desktop:text-xl outline-1 outline-primary mb-8 "
                 type="text"
                 value={name}
                 onChange={handleName}
               />
             )}
             {nameError ? (
-              <p className="absolute top-[-12px] left-[34.4px] px-[18.2px] bg-white font-nunito font-regular text-[13.76px] text-red-600 ">
+              <p className="absolute top-[-8px] laptop:top-[-8px] desktop:top-[-12px] left-[20px] px-[10px] desktop:left-[34.4px] desktop:px-[18.2px] text-[10px] laptop:text-[11px] desktop:text-[13.76px]  bg-white font-nunito font-regular  text-red-600 ">
                 Full name
               </p>
             ) : (
-              <p className="absolute top-[-12px] left-[34.4px] px-[18.2px] bg-white font-nunito font-regular text-[13.76px] text-[#11175D] text-opacity-[70%]">
+              <p className="absolute top-[-8px] laptop:top-[-8px] desktop:top-[-12px] left-[20px] px-[10px] laptop:left-[25px] desktop:left-[34.4px] desktop:px-[18.2px] text-[10px] laptop:text-[11px] desktop:text-[13.76px] bg-white font-nunito font-regular  text-[#11175D] text-opacity-[70%]">
                 Full name
               </p>
             )}
           </div>
 
           {/* Password input part here */}
-          <div className="relative w-[368px]">
+          <div className="relative w-full laptop:w-[280px] desktop:w-[368px]">
             {passwordError ? (
               <input
-                className=" border-[1.72px] border-solid rounded-lg border-red-600 w-[368px] h-20 pl-[52px] font-nunito font-semibold text-red-600 text-xl  mb-14  outline-none placeholder:text-red-600"
+                className=" border-[1.72px] border-solid rounded-lg border-red-600 w-full laptop:w-[280px] desktop:w-[368px]  py-[10px] md:py-[15px] px-[30px] desktop:h-20 desktop:pl-[52px] laptop:pr-[48px] desktop:pr-[63px] font-nunito font-semibold text-red-600 text-[13px] laptop:text-[15px] desktop:text-xl outline-none mb-8 placeholder:text-red-600 "
                 type={!showpassword ? "text" : "password"}
                 value={password}
                 placeholder={passwordError}
@@ -179,18 +179,18 @@ function Registration() {
               />
             ) : (
               <input
-                className=" border-[1.72px] border-solid rounded-lg border-[#11175D] w-[368px] border-opacity-[0.3] h-20 pl-[52px] pr-[60px] font-nunito font-semibold text-[#11175D] text-xl  mb-14 outline-primary outline-1"
+                className=" border-[1.72px] border-solid rounded-lg border-[#11175D] w-full laptop:w-[280px] desktop:w-[368px] border-opacity-[0.3] py-[10px] md:py-[15px] px-[30px] desktop:h-20 laptop:pr-[48px] desktop:pl-[52px] desktop:pr-[63px] font-nunito font-semibold text-[#11175D] text-[13px] laptop:text-[15px] desktop:text-xl outline-1 outline-primary mb-8 "
                 type={!showpassword ? "text" : "password"}
                 value={password}
                 onChange={handlePassword}
               />
             )}
             {passwordError ? (
-              <p className="absolute top-[-12px] left-[34.4px] px-[18.2px] bg-white font-nunito font-regular text-[13.76px] text-red-600 ">
+              <p className="absolute top-[-8px] laptop:top-[-8px] desktop:top-[-12px] left-[20px] px-[10px] desktop:left-[34.4px] desktop:px-[18.2px] text-[10px] laptop:text-[11px] desktop:text-[13.76px]  bg-white font-nunito font-regular  text-red-600 ">
                 Password
               </p>
             ) : (
-              <p className="absolute top-[-12px] left-[34.4px] px-[18.2px] bg-white font-nunito font-regular text-[13.76px] text-[#11175D] text-opacity-[70%]">
+              <p className="absolute top-[-8px] laptop:top-[-8px] desktop:top-[-12px] left-[20px] px-[10px] laptop:left-[25px] desktop:left-[34.4px] desktop:px-[18.2px] text-[10px] laptop:text-[11px] desktop:text-[13.76px] bg-white font-nunito font-regular  text-[#11175D] text-opacity-[70%]">
                 Password
               </p>
             )}
@@ -198,12 +198,12 @@ function Registration() {
               !showpassword ? (
                 <HiEye
                   onClick={() => setShowPassword(!showpassword)}
-                  className="absolute top-[25px] right-[25px] text-[26.38px] text-[#03014C] opacity-50 cursor-pointer "
+                  className="absolute top-[14px] md:top-[18px]  right-[15px] laptop:right-[20px] text-[15px] laptop:text-[18px] laptop:top-[18px] desktop:top-[25px] desktop:right-[25px] desktop:text-[26.38px] text-[#03014C] opacity-50 cursor-pointer "
                 />
               ) : (
                 <HiEyeOff
                   onClick={() => setShowPassword(!showpassword)}
-                  className="absolute top-[25px] right-[25px] text-[26.38px] text-[#03014C] opacity-50 cursor-pointer"
+                  className="absolute top-[14px]  md:top-[18px]  right-[15px] laptop:right-[20px] text-[15px] laptop:text-[18px] laptop:top-[18px] desktop:top-[25px] desktop:right-[25px] desktop:text-[26.38px] text-[#03014C] opacity-50 cursor-pointer"
                 />
               )
             ) : (
@@ -211,16 +211,16 @@ function Registration() {
             )}
           </div>
           <button
-            className="w-[368px] h-20 bg-primary rounded-full  font-nunito font-regular text-[20.64px] text-white mb-[35px]"
+            className="w-full laptop:w-[280px] desktop:w-[368px] py-[10px] md:py-[15px] px-[30px] desktop:h-20 laptop:pl-[52px] bg-primary rounded-full  font-nunito font-regular text-[15px] desktop:text-[20.64px] text-white mb-[35px]"
             onClick={handleClick}
           >
             Sign up
           </button>
-          <p className="font-opensans text-[13.34px] text-[#03014C] font-regular text-center w-[368px]">
+          <p className="font-opensans text-[10px] laptop:text-[13.34px] text-[#03014C] font-regular text-center w-full laptop:w-[280px] desktop:w-[368px]">
             Already have an account ?{" "}
             <Link
               to={"/login"}
-              className="font-opensans text-[13.34px] text-[#EA6C00] cursor-pointer font-bold"
+              className="font-opensans text-[10px] laptop:text-[13.34px] text-[#EA6C00] cursor-pointer font-bold"
             >
               Sign In
             </Link>
@@ -229,10 +229,10 @@ function Registration() {
       </div>
 
       {/* Right-side */}
-      <div className="w-2/4">
+      <div className="w-2/4 hidden md:block ">
         <picture>
           <img
-            className="h-screen w-full object-cover"
+            className="h-screen w-screen object-cover"
             src="images/registration-img.webp"
             alt="registration-img"
             loading="lazy"
